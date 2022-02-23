fe3d1132bceac2637fc86df6a854918865630e75
mykola <bubabum.drt@gmail.com>
2022-02-24 00:33:15 +0200

    added grid layout, active tiles

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function addPlane() {
 	let planeWidth = Number(document.getElementById('planeWidth').value);
 	let planeHeight = Number(document.getElementById('planeHeight').value);
 	plane = {
-		width: 5300,
+		width: 3000,
 		height: 2850,
 		excisions: [],
 		tiles: [],
@@ -86,16 +86,13 @@ function addTiles() {
 				y: j,
 				column: column,
 				row: row,
+				isActive: false,
 			};
 			row++;
 			plane.tiles.push(newTile);
 		}
 		column++;
 	}
-	// plane.tiles.forEach(element => {
-	// 	element.x += config.dX;
-	// 	element.y += config.dY;
-	// })
 	renderCanvas();
 }
 
@@ -109,6 +106,30 @@ function checkExcisions() {
 				tile.del = true;
 			}
 		});
+	}
+}
+
+function setTileGrid() {
+	const columns = Number(document.getElementById('columns').value);
+	const rows = Number(document.getElementById('rows').value);
+	const tileWidth = Math.floor((plane.width - config.tile.joint) / columns) + config.tile.joint;
+	const tileHeight = Math.floor((plane.height - config.tile.joint) / rows) + config.tile.joint;
+	config.tile.width = tileWidth - tileWidth % 5;
+	config.tile.height = tileHeight - tileHeight % 5;
+	config.dX = 0;
+	config.dY = 0;
+	addTiles();
+	checkGrid(columns, rows);
+	renderCanvas();
+}
+
+function checkGrid(columns, rows) {
+	for (let i = 0; i < plane.tiles.length; i++) {
+		let tile = plane.tiles[i];
+		tile.del = false;
+		if (tile.column === columns || tile.row === rows) {
+			tile.del = true;
+		}
 	}
 }
 
@@ -131,6 +152,61 @@ function moveTiles(btn) {
 	addTiles();
 }
 
+function trim() {
+	trimLeft();
+	trimRight();
+	trimUp();
+	trimDown();
+	plane.tiles.forEach((item) => {
+		if (item.width < 120 || item.height < 120) {
+			item.del = true;
+		}
+	})
+	renderCanvas();
+}
+
+function trimLeft() {
+	plane.tiles.forEach((element) => {
+		if (element.column === 0) {
+			element.width -= canvasPaddingX - element.x;
+			element.x = canvasPaddingX;
+		}
+	})
+	renderCanvas();
+}
+
+function trimRight() {
+	let firstColumnWidth = plane.tiles.find(item => item.column === 0).width;
+	plane.tiles.forEach((element) => {
+		if (element.column === Math.max(...plane.tiles.map(item => item.column))) {
+			element.width = canvasPaddingX + plane.width - element.x;
+			if (element.width > firstColumnWidth) element.width -= 5;
+		}
+	})
+	renderCanvas();
+}
+
+function trimUp() {
+	plane.tiles.forEach((element) => {
+		if (element.row === 0) {
+			element.height -= canvasPaddingY - element.y;
+			element.y = canvasPaddingY;
+		}
+	})
+	renderCanvas();
+}
+
+function trimDown() {
+	let firstRowHeight = plane.tiles.find(item => item.row === 0).height;
+	plane.tiles.forEach((element) => {
+		if (element.row === Math.max(...plane.tiles.map(item => item.row))) {
+			element.height = canvasPaddingY + plane.height - element.y;
+			if (element.height > firstRowHeight) element.height -= 5;
+		}
+	})
+	renderCanvas();
+}
+
 function centerX() {
 	let tilesWidth = plane.tiles.filter((element) => element.row === 0).map((element) => element.width).reduce((previousValue, currentValue) => previousValue + currentValue - config.tile.joint);
 	config.dX = Math.floor(((canvasPaddingX * 2) + plane.width - tilesWidth) / 20) * 10 - canvasPaddingX;
@@ -139,7 +215,7 @@ function centerX() {
 
 function centerY() {
 	let tilesHeight = plane.tiles.filter((element) => element.column === 0).map((element) => element.height).reduce((previousValue, currentValue) => previousValue + currentValue - config.tile.joint);
-	config.dY = Math.floor(((canvasPaddingY * 2) + plane.height - tilesHeight) / 20) * 10 - canvasPaddingY;
+	config.dY = Math.floor(((canvasPaddingY * 2) + plane.height - tilesHeight) / 20) * 10 - canvasPaddingY + 5;
 	addTiles();
 }
 
@@ -165,6 +241,9 @@ function renderCanvas() {
 	ctx.font = "120px san-serif";
 	ctx.fillText(`${plane.width}`, canvasPaddingX + plane.width / 2, canvasPaddingY / 2);
 	ctx.fillText(`${plane.height}`, canvasPaddingX / 2, canvasPaddingY + plane.height / 2);
+	plane.tiles.forEach(item => {
+		renderActiveTile(item);
+	});
 }
 
 function renderExcisionBtn() {
@@ -174,11 +253,10 @@ function renderExcisionBtn() {
 	for (let i = 0; i < plane.excisions.length; i++) {
 		let btn = document.createElement('button');
 		btn.dataset.id = i;
-		btn.innerHTML = `Видалити виріз ${i + 1}: ${plane.excisions[i].width} ${plane.excisions[i].height} ${plane.excisions[i].dX} ${plane.excisions[i].dY}`;
+		btn.innerHTML = `<i class="fa-solid fa-trash"></i> Видалити виріз ${i + 1}: ${plane.excisions[i].width} ${plane.excisions[i].height} ${plane.excisions[i].dX} ${plane.excisions[i].dY}`;
 		excisions.appendChild(btn);
 	}
 }
-
 
 function renderExcision(excision) {
 	ctx.strokeStyle = "#fcba03";
@@ -189,13 +267,17 @@ function renderTile(tile) {
 	if (tile.del === true) return
 	ctx.fillStyle = "#DDDDDD";
 	ctx.fillRect(tile.x, tile.y, tile.width, tile.height);
-	// ctx.strokeStyle = "#DDDDDD";
-	// ctx.strokeRect(tile.x, tile.y, tile.width, tile.height);
 	ctx.fillStyle = "#FAFAFA";
 	ctx.fillRect(tile.x + 15, tile.y + 15, tile.width - 30, tile.height - 30);
 	ctx.fillStyle = "#000000";
 	ctx.font = "60px san-serif";
-	ctx.fillText(`${tile.width} x ${tile.height}`, tile.x + 100, tile.y + tile.height / 2);
+	ctx.fillText(`${tile.width}x${tile.height}`, tile.x + 100, tile.y + tile.height / 2);
+}
+
+function renderActiveTile(tile) {
+	if (!tile.isActive) return
+	ctx.strokeStyle = "#79a884";
+	ctx.strokeRect(tile.x + 15, tile.y + 15, tile.width - 30, tile.height - 30);
 }
 
 function deleteExcision(id) {
@@ -210,6 +292,31 @@ function setTile() {
 	config.tile.width = tileWidth;
 	config.tile.height = tileHeight;
 	config.tile.joint = tileJoint;
+}
+
+function getTile(event, canvas) {
+	let x = event.offsetX / parseInt(window.getComputedStyle(canvas).width) * (plane.width + canvasPaddingX * 2);
+	let y = event.offsetY / parseInt(window.getComputedStyle(canvas).height) * (plane.height + canvasPaddingY * 2);
+	plane.tiles.forEach(item => {
+		if (item.x < x && item.x + item.width > x && item.y < y && item.y + item.height > y) {
+			item.isActive = !item.isActive;
+		}
+	})
+	renderCanvas();
+}
+
+function sizeLeft() {
+	let column = plane.tiles.find(item => item.isActive === true).column;
+	plane.tiles.forEach(item => {
+		if (item.column === column) {
+			item.width += 10;
+		}
+		if (item.column === column + 1) {
+			item.x += 10;
+			item.width -= 10;
+		}
+	})
+	renderCanvas();
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
@@ -233,6 +340,27 @@ document.addEventListener('DOMContentLoaded', function (event) {
 		setTile();
 		addTiles();
 	});
+	document.getElementById('columns').addEventListener('input', function (event) {
+		setTileGrid();
+	});
+	document.getElementById('rows').addEventListener('input', function (event) {
+		setTileGrid();
+	});
+	document.getElementById('trim').addEventListener('click', function (event) {
+		trim();
+	});
+	document.getElementById('trimLeft').addEventListener('click', function (event) {
+		trimLeft();
+	});
+	document.getElementById('trimRight').addEventListener('click', function (event) {
+		trimRight();
+	});
+	document.getElementById('trimUp').addEventListener('click', function (event) {
+		trimUp();
+	});
+	document.getElementById('trimDown').addEventListener('click', function (event) {
+		trimDown();
+	});
 	document.querySelector('.control__step').addEventListener('click', function (event) {
 		if (event.target.classList.contains('step')) changeStep(event.target.value);
 	});
@@ -245,4 +373,11 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	document.getElementById('centerY').addEventListener('click', function (event) {
 		centerY();
 	});
+	document.getElementById('sizeLeft').addEventListener('click', function (event) {
+		sizeLeft();
+	});
+	document.querySelector('.canvas').addEventListener('click', function (event) {
+		getTile(event, this);
+	});
+
 });
